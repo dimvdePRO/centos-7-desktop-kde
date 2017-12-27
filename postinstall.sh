487a1a56c4aef35d2aa2c319c3fa3fc1da1ba117
Nicolas Kovacs <info@microlinux.fr>
2017-12-27 17:33:44 +0100

    Un peu de rangement.

--- postinstall.sh
+++ postinstall.sh
@@ -21,7 +21,7 @@ echo "::"
 echo ":: Configuration de Bash pour l'administrateur..."
 echo "::"
 sleep $DELAY
-cat $CWD/bash/bashrc-root > /root/.bashrc
+cat $CWD/config/bash/bashrc-root > /root/.bashrc
 echo -e ":: [${VERT}OK${GRIS}]"
 echo "::"
 sleep $DELAY
@@ -29,7 +29,7 @@ sleep $DELAY
 echo ":: Configuration de Bash pour les utilisateurs..."
 echo "::"
 sleep $DELAY
-cat $CWD/bash/bashrc-users > /etc/skel/.bashrc
+cat $CWD/config/bash/bashrc-users > /etc/skel/.bashrc
 echo -e ":: [${VERT}OK${GRIS}]"
 echo "::"
 sleep $DELAY
@@ -37,7 +37,7 @@ sleep $DELAY
 echo ":: Configuration de Vim..."
 echo "::"
 sleep $DELAY
-cat $CWD/vim/vimrc > /etc/vimrc
+cat $CWD/config/vim/vimrc > /etc/vimrc
 echo -e ":: [${VERT}OK${GRIS}]"
 echo "::"
 sleep $DELAY
@@ -45,7 +45,7 @@ sleep $DELAY
 echo ":: Configuration des dépôts de base..."
 echo "::"
 sleep $DELAY
-cat $CWD/yum/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
+cat $CWD/config/yum/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
 echo -e ":: [${VERT}OK${GRIS}]"
 echo "::"
 sleep $DELAY
@@ -53,7 +53,7 @@ sleep $DELAY
 echo ":: Configuration du dépôt CR..."
 echo "::"
 sleep $DELAY
-cat $CWD/yum/CentOS-CR.repo > /etc/yum.repos.d/CentOS-CR.repo
+cat $CWD/config/yum/CentOS-CR.repo > /etc/yum.repos.d/CentOS-CR.repo
 echo -e ":: [${VERT}OK${GRIS}]"
 echo "::"
 sleep $DELAY
@@ -71,8 +71,8 @@ if ! rpm -q epel-release 2>&1 > /dev/null ; then
   echo ":: Configuration du dépôt EPEL..."
   echo "::"
   yum -y install epel-release 1> /dev/null
-  cat $CWD/yum/epel.repo > /etc/yum.repos.d/epel.repo
-  cat $CWD/yum/epel-testing.repo > /etc/yum.repos.d/epel-testing.repo
+  cat $CWD/config/yum/epel.repo > /etc/yum.repos.d/epel.repo
+  cat $CWD/config/yum/epel-testing.repo > /etc/yum.repos.d/epel-testing.repo
   echo -e ":: [${VERT}OK${GRIS}]"
   echo "::"
   sleep $DELAY
@@ -82,7 +82,7 @@ if ! rpm -q nux-dextop-release 2>&1 > /dev/null ; then
   echo ":: Configuration du dépôt Nux-Dextop..."
   echo "::"
   yum -y localinstall $CWD/yum/nux-dextop-release-*.rpm 1> /dev/null
-  cat $CWD/yum/nux-dextop.repo > /etc/yum.repos.d/nux-dextop.repo
+  cat $CWD/config/yum/nux-dextop.repo > /etc/yum.repos.d/nux-dextop.repo
   echo -e ":: [${VERT}OK${GRIS}]"
   echo "::"
   sleep $DELAY
@@ -92,7 +92,7 @@ if ! rpm -q adobe-release-x86_64 2>&1 > /dev/null ; then
   echo ":: Configuration du dépôt Adobe..."
   echo "::"
   yum -y localinstall $CWD/yum/adobe-release-*.rpm 1> /dev/null
-  cat $CWD/yum/adobe-linux-x86_64.repo > /etc/yum.repos.d/adobe-linux-x86_64.repo
+  cat $CWD/config/yum/adobe-linux-x86_64.repo > /etc/yum.repos.d/adobe-linux-x86_64.repo
   echo -e ":: [${VERT}OK${GRIS}]"
   echo "::"
   sleep $DELAY
@@ -103,7 +103,7 @@ if ! rpm -q elrepo-release 2>&1 > /dev/null ; then
   echo "::"
   rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org 1> /dev/null
   yum -y localinstall $CWD/yum/elrepo-release-*.rpm 1> /dev/null
-  cat $CWD/yum/elrepo.repo > /etc/yum.repos.d/elrepo.repo
+  cat $CWD/config/yum/elrepo.repo > /etc/yum.repos.d/elrepo.repo
   echo -e ":: [${VERT}OK${GRIS}]"
   echo "::"
   sleep $DELAY
