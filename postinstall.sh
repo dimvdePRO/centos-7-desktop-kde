46c9ffa879824eff3179766fd91056944e129dc1
Niki Kovacs <info@microlinux.fr>
2018-01-01 17:31:00 +0100

    Message pour la création des utilisateurs.

--- postinstall.sh
+++ postinstall.sh
@@ -41,9 +41,8 @@ echo "     ### CentOS 7 KDE Post-installation ###" | tee -a $LOG
 echo "     ######################################" | tee -a $LOG
 echo | tee -a $LOG
 sleep $DELAY
-echo "     Pour suivre l'avancement des opérations,"
-echo "     ouvrez une deuxième console et invoquez "
-echo "     la commande suivante :"
+echo "     Pour suivre l'avancement des opérations, ouvrez une"
+echo "     deuxième console et invoquez la commande suivante :"
 echo
 echo "       # tail -f /tmp/postinstall.log"
 echo
@@ -55,9 +54,9 @@ yum -y update >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Pour l'instant on n'utilise que l'IPv4
+echo "::"
 echo -e ":: Désactivation de l'IPv6... \c"
 sleep $DELAY
 cat $CWD/config/sysctl.d/disable-ipv6.conf > /etc/sysctl.d/disable-ipv6.conf
@@ -68,36 +67,36 @@ fi
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Personnalisation du shell Bash pour root
+echo "::"
 echo -e ":: Configuration du shell Bash pour l'administrateur... \c"
 sleep $DELAY
 cat $CWD/config/bash/bashrc-root > /root/.bashrc 
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Personnalisation du shell Bash pour les utilisateurs
+echo "::"
 echo -e ":: Configuration du shell Bash pour les utilisateurs... \c"
 sleep $DELAY
 cat $CWD/config/bash/bashrc-users > /etc/skel/.bashrc
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Quelques options pratiques pour Vim
+echo "::"
 echo -e ":: Configuration de Vim... \c"
 sleep $DELAY
 cat $CWD/config/vim/vimrc > /etc/vimrc
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Activer les dépôts [base], [updates] et [extras] avec une priorité de 1
+echo "::"
 echo -e ":: Configuration des dépôts de paquets officiels... \c"
 sleep $DELAY
 cat $CWD/config/yum/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
@@ -105,29 +104,29 @@ sed -i -e 's/installonly_limit=5/installonly_limit=2/g' /etc/yum.conf
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Activer le dépôt [cr] avec une priorité de 1
+echo "::"
 echo -e ":: Configuration du dépôt de paquets CR... \c"
 sleep $DELAY
 cat $CWD/config/yum/CentOS-CR.repo > /etc/yum.repos.d/CentOS-CR.repo
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Installer le plugin Yum-Priorities
 if ! rpm -q yum-plugin-priorities 2>&1 > /dev/null ; then
+  echo "::"
   echo -e ":: Installation du plugin Yum-Priorities... \c"
   yum -y install yum-plugin-priorities >> $LOG 2>&1
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Activer le dépôt [epel] avec une priorité de 10
 if ! rpm -q epel-release 2>&1 > /dev/null ; then
+  echo "::"
   echo -e ":: Configuration du dépôt de paquets EPEL... \c"
   rpm --import http://download.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7 >> $LOG 2>&1
   yum -y install epel-release >> $LOG 2>&1
@@ -136,11 +135,11 @@ if ! rpm -q epel-release 2>&1 > /dev/null ; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Activer le dépôt [nux-dextop] avec une priorité de 10
 if ! rpm -q nux-dextop-release 2>&1 > /dev/null ; then
+  echo "::"
   echo -e ":: Configuration du dépôt de paquets Nux-Dextop... \c"
   yum -y localinstall $CWD/config/yum/nux-dextop-release-*.rpm >> $LOG 2>&1
   rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-nux.ro >> $LOG 2>&1
@@ -148,11 +147,11 @@ if ! rpm -q nux-dextop-release 2>&1 > /dev/null ; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Activer le dépôt [adobe-linux-x86_64] avec une priorité de 10
 if ! rpm -q adobe-release-x86_64 2>&1 > /dev/null ; then
+  echo "::"
   echo -e ":: Configuration du dépôt de paquets Adobe... \c"
   yum -y localinstall $CWD/config/yum/adobe-release-*.rpm >> $LOG 2>&1
   rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux >> $LOG 2>&1
@@ -160,11 +159,11 @@ if ! rpm -q adobe-release-x86_64 2>&1 > /dev/null ; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Configurer les dépôts [elrepo], [elrepo-kernel], etc. sans les activer
 if ! rpm -q elrepo-release 2>&1 > /dev/null ; then
+  echo "::"
   echo -e ":: Configuration du dépôt de paquets ELRepo... \c"
   rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org >> $LOG 2>&1
   yum -y localinstall $CWD/config/yum/elrepo-release-*.rpm >> $LOG 2>&1
@@ -172,66 +171,66 @@ if ! rpm -q elrepo-release 2>&1 > /dev/null ; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Synchroniser les dépôts de paquets
+echo "::"
 echo -e ":: Synchronisation des dépôts de paquets... \c"
 yum check-update >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Installer les outils Linux listés dans config/pkglists/outils-linux.txt
+echo "::"
 echo -e ":: Installation des outils système Linux... \c"
 PAQUETS=$(egrep -v '(^\#)|(^\s+$)' $CWD/config/pkglists/outils-linux.txt)
 yum -y install $PAQUETS >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Supprimer les paquets inutiles listés dans config/pkglists/cholesterol.txt
+echo "::"
 echo -e ":: Suppression des paquets inutiles... \c"
 CHOLESTEROL=$(egrep -v '(^\#)|(^\s+$)' $CWD/config/pkglists/cholesterol.txt)
 yum -y remove $CHOLESTEROL >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Installer les paquets listés dans config/pkglists/bureau-kde.txt
+echo "::"
 echo -e ":: Installation des applications supplémentaires... \c"
 PAQUETS=$(egrep -v '(^\#)|(^\s+$)' $CWD/config/pkglists/bureau-kde.txt)
 yum -y install $PAQUETS >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Personnaliser les entrées du menu KDE
+echo "::"
 echo -e ":: Personnalisation des entrées de menu KDE... \c"
 sleep $DELAY
 $CWD/menus.sh >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Installer le profil par défaut des utilisateurs
+echo "::"
 echo -e ":: Installation du profil par défaut des utilisateurs... \c"
 sleep $DELAY
 $CWD/profil.sh >> $LOG 2>&1
 echo -e "[${VERT}OK${GRIS}] \c"
 sleep $DELAY
 echo
-echo "::"
 
 # Installer les polices Apple
 if [ ! -d /usr/share/fonts/apple-fonts ]; then
   cd /tmp
   rm -rf /usr/share/fonts/apple-fonts
+  echo "::"
   echo -e ":: Installation des polices TrueType Apple... \c"
   wget -c https://www.microlinux.fr/download/FontApple.tar.xz >> $LOG 2>&1
   mkdir /usr/share/fonts/apple-fonts
@@ -242,13 +241,13 @@ if [ ! -d /usr/share/fonts/apple-fonts ]; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Installer la police Eurostile
 if [ ! -d /usr/share/fonts/eurostile ]; then
   cd /tmp
   rm -rf /usr/share/fonts/eurostile
+  echo "::"
   echo -e ":: Installation de la police TrueType Eurostile... \c"
   wget -c https://www.microlinux.fr/download/Eurostile.zip >> $LOG 2>&1
   unzip Eurostile.zip -d /usr/share/fonts/ >> $LOG 2>&1
@@ -258,12 +257,12 @@ if [ ! -d /usr/share/fonts/eurostile ]; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
 
 # Installer les fonds d'écran Microlinux
 if [ ! -f /usr/share/backgrounds/.microlinux ]; then
   cd /tmp
+  echo "::"
   echo -e ":: Installation des fonds d'écran Microlinux... \c"
   wget -c https://www.microlinux.fr/download/microlinux-wallpapers.tar.gz >> $LOG 2>&1
   tar xvzf microlinux-wallpapers.tar.gz >> $LOG 2>&1 
@@ -273,8 +272,21 @@ if [ ! -f /usr/share/backgrounds/.microlinux ]; then
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
-  echo "::"
 fi
+
+# Avertissement final
+echo
+echo "     L'installation du bureau est terminée. À présent," 
+echo "     vous pouvez créer un ou plusieurs utilisateurs :"
+echo
+echo "       # useradd -c \"Prénom Nom\" utilisateur"
+echo "       # passwd utilisateur"
+echo
+echo "     Redémarrez le PC et supprimez l'utilisateur initial :"
+echo
+echo "       # userdel -r install"
+echo
+sleep $DELAY
 
 echo
 
